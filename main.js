5f0acf34d69452da624e8c9d46c4b1fdb3a64949
HP Laptop <isroilovbakhtinur@gmail.com>
2025-04-23 12:26:34 +0500

    JS kirish

---
+++ main.js
@@ -0,0 +1,32 @@
+var question1 = prompt("HTMlda sarlavha taglari qaysilar?")
+var question2 = prompt("Inline taglar qaysilar?")
+var question3 = prompt("Span qanday element?")
+var question4 = prompt("Attribute nmalardan tashkil topgan?")
+var question5 = prompt("Css ni HTML ga ulashni necha xil usuli bor?")
+var question6 = prompt("Saytdagi asosiy kontentlarni urtaga olib kursatuvchi element . . .?")
+var question7 = prompt("div va span elementlarining farqi ?")
+var question8 = prompt("HTML da alt atributi qaysi tag bilan iwlatiladi?")
+var question9 = prompt("HTML da input elementi nima uchun ishlatiladi?")
+var question10 = prompt("Flexbox nma uchun ishlatiladi?")
+var question11 = prompt("Mobile-first element deganda nma tushuniladi?")
+var question12 = prompt("Css da padding vazifasi ...")
+var question13 = prompt("HTML da placeholder attributi qayerda ishlatiladi?")
+var question14 = prompt("Css da Box-model qanday qismlardan iborat?")
+var question15 = prompt("CSS Box Modelda box-sizing: border-box; nima qiladi?")
+
+
+console.log(question1);
+console.log(question2);
+console.log(question3);
+console.log(question4);
+console.log(question5);
+console.log(question6);
+console.log(question7);
+console.log(question8);
+console.log(question9);
+console.log(question10);
+console.log(question11);
+console.log(question12);
+console.log(question13);
+console.log(question14);
+console.log(question15);
